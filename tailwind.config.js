5dbd08630833428ef66d78348b39bd52ca1052b7
Brandon Cordell <brandon@brandoncordell.com>
2025-01-17 15:57:41 -0500

    Extend tailwind colors
    Using the official `#009859`, I generated a tailwind-style list of greens.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
   content: [
     './app/helpers/**/*.rb',
@@ -10,6 +13,25 @@ export default {
 
   theme: {
     extend: {
+      colors: {
+        'dg-green': {
+          DEFAULT: '#009859',
+          50: '#ebfef3',
+          100: '#d0fbe1',
+          200: '#a4f6c7',
+          300: '#6aebab',
+          400: '#2fd888',
+          500: '#0abf70',
+          600: '#009859',
+          700: '#007c4c',
+          800: '#03623d',
+          900: '#045034',
+          950: '#012d1e'
+        },
+
+        smoke: '#231f20'
+      },
+
       fontFamily: {
         sans: [
           '"Inter", sans-serif',
